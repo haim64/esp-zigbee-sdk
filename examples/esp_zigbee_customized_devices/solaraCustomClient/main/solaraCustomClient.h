1b96d733b99ea1dae447dfc54ecca30b232498a8
haim alon <haim.alon@gmail.com>
2025-01-06 09:00:34 +0200

    my

---
+++ examples/esp_zigbee_customized_devices/solaraCustomClient/main/solaraCustomClient.h
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Zigbee customized client Example
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+#include "esp_zigbee_core.h"
+#include "switch_driver.h"
+
+/* Zigbee configuration */
+#define INSTALLCODE_POLICY_ENABLE       false    /* enable the install code policy for security */
+#define ED_AGING_TIMEOUT                ESP_ZB_ED_AGING_TIMEOUT_64MIN
+#define ED_KEEP_ALIVE                   3000    /* 3000 millisecond */
+#define HA_ONOFF_SWITCH_ENDPOINT        1           /* esp switch device endpoint */
+#define ESP_ZB_PRIMARY_CHANNEL_MASK     (1l << 11)  /* Zigbee primary channel mask use in the example */
+#define ESP_ZB_SECONDARY_CHANNEL_MASK   (1l << 13)  /* Zigbee primary channel mask use in the example */
+
+#define CUSTOM_DEVICE_ID (0xFFF2)   // ZB_HA_CUSTOM_ATTR_DEVICE_ID
+#define CUSTOM_CLUSTER_ID 0xff00
+#define CUSTOM_COMMAND_REQ 0x0000
+
+/* Basic manufacturer information */
+#define ESP_MANUFACTURER_NAME "\x09""SOLARAINC"     /* Customized manufacturer name */
+#define ESP_MODEL_IDENTIFIER "\x08""SOLCUSTC"       /* Customized model identifier */
+
+#define ESP_ZB_ZED_CONFIG()                                         \
+    {                                                               \
+        .esp_zb_role = ESP_ZB_DEVICE_TYPE_ED,                       \
+        .install_code_policy = INSTALLCODE_POLICY_ENABLE,           \
+        .nwk_cfg.zed_cfg = {                                        \
+            .ed_timeout = ED_AGING_TIMEOUT,                         \
+            .keep_alive = ED_KEEP_ALIVE,                            \
+        },                                                          \
+    }
+
+#define ESP_ZB_DEFAULT_RADIO_CONFIG()                           \
+    {                                                           \
+        .radio_mode = ZB_RADIO_MODE_NATIVE,                     \
+    }
+
+#define ESP_ZB_DEFAULT_HOST_CONFIG()                            \
+    {                                                           \
+        .host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,   \
+    }
